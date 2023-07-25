c269395844bf65fa683d9109c419d1bb007b2d84
Navid <navidjalili3@gmail.com>
2023-07-25 19:32:46 +0330

    Update Program.cs

--- Console.UI/Program.cs
+++ Console.UI/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Choose 'client' or 'server':");
+string choice = Console.ReadLine() ?? "";
+
+if (choice.ToLower() == "client")
+{
+    var clientLogic = new UDP.Library.Client();
+    while (true)
+    {
+        clientLogic.RunClient();
+    }
+}
+else if (choice.ToLower() == "server")
+{
+    var serverLogic = new UDP.Library.Server();
+    while (true)
+    {
+        serverLogic.RunServer();
+    }
+}
+else
+{
+    Console.WriteLine("Invalid choice.");
+}
