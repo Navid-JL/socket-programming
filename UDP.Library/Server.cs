cac262c346b6dc0558a23c4e66cbec8c02ad4394
Navid <navidjalili3@gmail.com>
2023-07-25 19:30:14 +0330

    Implement server side (todo #2)
    The server receives the data and converts the characters to uppercase.

---
+++ UDP.Library/Server.cs
@@ -0,0 +1,26 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDP.Library;
+
+public class Server
+{
+    public void RunServer()
+    {
+        int serverPort = 5000;
+
+        using (var server = new UdpClient(serverPort))
+        {
+            IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            while (true)
+            {
+                byte[] receivedData = server.Receive(ref clientEndPoint);
+
+                string inputData = Encoding.ASCII.GetString(receivedData);
+                string modifiedData = inputData.ToUpper();
+            }
+        }
+    }
+}
